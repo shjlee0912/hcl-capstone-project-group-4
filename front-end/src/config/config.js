3a48499cf52e4418e4dc34b6d51a8ea42dd9b7d3
Gabriel Hartmark <gabriel_hartmark@hcl.com>
2021-09-05 19:34:14 -0400

    removed Procfile, configured for deployment

--- front-end/src/config/config.js
+++ front-end/src/config/config.js
@@ -1,10 +1,10 @@
 
 const dev = {
-    API_URL: "http://localhost:9191"
+    API_URL: process.env.REACT_APP_API_URL || "http://localhost:9191"
 }
 
 const prod = {
-    API_URL: process.env.API_URL
+    API_URL: process.env.REACT_APP_API_URL
 }
 
 const CONFIG = process.env.REACT_APP_STAGE==="prod"?prod:dev;
