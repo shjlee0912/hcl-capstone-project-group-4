a4fb349dfb63b958a3a7586144eebcb425b7091f
Hyunjay Lee <hyunjay_lee@hcl.com>
2021-09-07 10:45:22 -0400

    new product

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
-import cartReducer from './cartSlice';
-import catalogReducers from './catalogSlice';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from 'redux-thunk';
+import rootReducer from './redux';
 
-export default configureStore({
-    reducer: {
-        auth: authReducer,
-        cart: cartReducer,
-        catalog: catalogReducers,
-    },
-});
+const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
