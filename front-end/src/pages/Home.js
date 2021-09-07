b1441a260da418a3cb768a877bce9249e88faa49
danielpintor <86312023+danielpintor@users.noreply.github.com>
2021-09-07 11:27:58 -0400

    Update Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const Home = (props) => {
-    return (
-        "homepage"
-    );
-};
+import TopBar from '../components/TopBar';
+import homepage from '../components/homepage';
+
+
+function Home() {
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/*  Site header */}
+      <TopBar />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+
+        {/*  Page sections */}
+        <homepage />
+
+      </main>
+
+    </div>
+  );
+}
 
 export default Home;
