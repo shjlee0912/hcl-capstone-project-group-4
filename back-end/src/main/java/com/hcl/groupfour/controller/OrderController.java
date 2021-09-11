4e855687111b10809e8e9e08d69b13a1ab6d320a
danielpintor <86312023+danielpintor@users.noreply.github.com>
2021-09-10 23:49:53 -0400

    Added logging

--- back-end/src/main/java/com/hcl/groupfour/controller/OrderController.java
+++ back-end/src/main/java/com/hcl/groupfour/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.hcl.groupfour.controller;
 
 import java.security.Principal;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,9 @@ import com.stripe.model.Charge;
 
 @RestController
 public class OrderController {
-
+	
+	private final static Logger log = LogManager.getLogger(OrderController.class);
+	
 	@Autowired
 	OrderService orderService;
 	
@@ -24,9 +28,11 @@ public class OrderController {
 	public ResponseEntity<Void> placeOrder(@RequestBody PlaceOrderDTO order, Principal principal) {
 		try {
 			Charge charge = orderService.makePayment(order, principal.getName());
+			log.info("INFO: Payment successful.");
 			return ResponseEntity.ok(null);
 		} catch(Exception e) {
 			e.printStackTrace();
+			log.error("ERROR: Order failed. Message: " + e.getMessage());
 			return ResponseEntity.status(500).build(); //more status codes should be added later
 		}
 	}
